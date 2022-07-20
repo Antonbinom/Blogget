82b053fb75f1bd92ecdfc00c297a7c9c29c2c2c3
Anton Semenikhin <protest_the_hero@mail.ru>
2022-07-20 19:45:53 +0300

    fix propTypes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,14 @@
 import style from './Layout.module.css';
-import PropTypes from 'prop-types';
+import PropTypes, {oneOfType} from 'prop-types';
 
 export const Layout = ({children}) => (<div className={style.container}>{children}</div>);
+
 Layout.propTypes = {
-  children: PropTypes.object,
+  children: oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.bool,
+    PropTypes.object,
+    PropTypes.array,
+  ])
 };
